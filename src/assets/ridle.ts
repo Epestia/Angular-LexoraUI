16a70da892fc53c8feb6e9c047bb3f8cd70ad877
Epestia <epestia.lumis@hotmail.com>
2026-06-08 00:55:59 +0200

    promouve admin and delete

---
+++ src/assets/ridle.ts
@@ -0,0 +1,34 @@
+
+
+class Riddle {
+  question: string;
+  answer: string;
+
+  constructor(question: string, answer: string) {
+    this.question = question;
+    this.answer = answer.toLowerCase().trim();
+  }
+
+  checkAnswer(userAnswer: string): boolean {
+    return userAnswer.toLowerCase().trim() === this.answer;
+  }
+
+  showQuestion(): void {
+    console.log(`🧩 Riddle: ${this.question}`);
+  }
+}
+
+const riddle1 = new Riddle(
+  "Je parle sans bouche et j'entends sans oreilles. Qui suis-je ?",
+  'echo',
+);
+
+riddle1.showQuestion();
+
+const userAnswer = 'Echo';
+
+if (riddle1.checkAnswer(userAnswer)) {
+  console.log('Bonne réponse !');
+} else {
+  console.log('Mauvaise réponse !');
+}
